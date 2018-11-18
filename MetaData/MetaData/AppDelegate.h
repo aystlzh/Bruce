d193e3fb769afccb555ad8d0ecdd95d98d47a815
大志 <lxllzh@gmail.com>
2018-11-18 23:50:53 +0800

    Add files via upload

---
+++ MetaData/MetaData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetaData
+//
+//  Created by wumingwei on 2018/11/17.
+//  Copyright © 2018年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MetaData/MetaData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetaData
+//
+//  Created by wumingwei on 2018/11/17.
+//  Copyright © 2018年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
